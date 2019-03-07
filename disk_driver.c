9c6036daf7de8d2d19ebc4b484190e4d11b12563
Lorenzo Bianchi <bianchi.1756496@studenti.uniroma1.it>
2019-03-07 16:23:50 +0100

    SImpleFileSystem v. 2.0 - disk driver fixed

--- disk_driver.c
+++ disk_driver.c
@@ -1,6 +1,7 @@
 #include "disk_driver.h"
 #include "error.h"
 #include "bitmap.h"
+#include <unistd.h>	
 #include <stdio.h>
 #include <stdlib.h> 
 #include <fcntl.h>
@@ -8,12 +9,29 @@
 #include <errno.h>
 #include <string.h>
 
-
+#define DEBUG 0
 #define BLOCK_SIZE 512
+
+static void DiskDriver_printInfo(DiskHeader* dh) {
+	printf("=========\n");
+	printf("DISK DRIVER INITIALISED ...\n");
+	printf("\t num_blocks : %d\n", dh -> num_blocks);
+	printf("\t bitmap_blocks : %d\n", dh -> bitmap_blocks);
+	printf("\t bitmap_entries : %d\n", dh -> bitmap_entries);
+	printf("\t free_blocks : %d\n", dh -> free_blocks);
+	printf("\t first_free_block (stored in header) : %d\n", dh -> first_free_block);
+	
+	
+	BitMap bitmap;
+	bitmap.num_bits = dh->num_blocks;
+	bitmap.entries = ((void*)dh)+sizeof(DiskHeader);
+	printf("\t first_free_block (actual in bitmap) : %d\n", BitMap_get(&bitmap, 0, 0));
+	printf("=========\n");
+}
 
 static void DiskDriver_initDiskHeader(DiskHeader* dh, int num_blocks, int entries, int free_blocks, int first_free_block){
 	dh -> num_blocks = num_blocks;
-	dh -> bitmap_blocks = (num_blocks + BLOCK_SIZE)/BLOCK_SIZE;  
+	dh -> bitmap_blocks = (num_blocks + BLOCK_SIZE-1)/BLOCK_SIZE;  
 	dh -> bitmap_entries = entries;  
   
 	dh -> free_blocks = free_blocks;     
@@ -24,62 +42,87 @@ static void DiskDriver_initDiskHeader(DiskHeader* dh, int num_blocks, int entrie
 void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks) {
 	int ret;
 	int fd = open(filename, O_CREAT | O_RDWR, 0600);
+	//prendo la size del file
+	off_t size_fs = lseek(fd, 0, SEEK_END);
+	if(DEBUG) printf("[DiskDriver_init]Opened File System \"%s\" of size %ld\n", filename, (long)size_fs);
+	lseek(fd, 0, SEEK_SET);
 	
-	int bitmap_size = num_blocks >> 3;
+	
+	int bitmap_size = (num_blocks+7) >> 3;
 	
 	int dd_size = sizeof(DiskHeader) + bitmap_size;
 	
-	void* dd_data = mmap(0, dd_size,  PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	
+	if(DEBUG) printf("[DiskDriver_init] size : %d\n",dd_size + num_blocks*BLOCK_SIZE);
+	
+	ret = posix_fallocate(fd, 0, dd_size + num_blocks*BLOCK_SIZE);
+	check_err(ret != 0, "fallocate failed\n");
+	
+	void* dd_data = mmap(0, dd_size + num_blocks*BLOCK_SIZE,  PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	check_err(!dd_data, "mmap failed\n");
+	
+	if(DEBUG) printf("[DiskDriver_init] dd_data mmaped at %p\n", dd_data);
 	
 	disk->header = (DiskHeader*)dd_data;
 	disk->bitmap_data = sizeof(DiskHeader) + (char*)dd_data;
 	disk->fd = fd;
 
-	if (fd < 0) {
-  		/* failure */
-  		if (errno == EEXIST) {
-    		/* the file already existed */
-			ret = posix_fallocate(disk->fd, disk->header->num_blocks * BLOCK_SIZE, dd_size);
-            check_err(ret != 0, "fallocate failed\n"); 
-
-            disk->header->bitmap_entries &= 0;
-		}
-	} else {
-  		ret = posix_fallocate(fd, 0, dd_size);
-	    check_err(ret != 0, "fallocate failed\n");
-	    /* init diskheader */
-	    DiskDriver_initDiskHeader(disk->header, num_blocks, bitmap_size, num_blocks, 0);
-	}	
+	if (size_fs == 0) {
+		/* init diskheader */
+    	DiskDriver_initDiskHeader(disk->header, num_blocks, bitmap_size, num_blocks, 0);
+    	*(disk->bitmap_data) &= 0;
+	}
+	DiskDriver_printInfo(disk->header);
 	
-	*(disk->bitmap_data) &= 0;
+	if(DEBUG) printf("[DiskDriver_init] disk->header at %p\n", disk->header);
 }
 
 
 int DiskDriver_readBlock(DiskDriver* disk, void* dest, int block_num) {
-	if(disk->bitmap_data[block_num] == 0 || block_num >= disk->header->num_blocks) {
+	if(!dest || block_num >= disk->header->num_blocks) {
+		printf("[DiskDriver_readBlock] invalid index of block to read : %d\n",block_num);
 		return -1;
-	} 
+	}
+
+	BitMap bitmap;
+	bitmap.num_bits = disk->header->num_blocks;
+	bitmap.entries = disk->bitmap_data;
+	//check if block in pos block_num is not free
+	int first_occupied_block = BitMap_get(&bitmap, block_num, 1);
+	if(first_occupied_block!=block_num) { //wanted block is free
+		printf("[DiskDriver_readBlock] wanted block is free : %d\n",block_num);
+		return -1;
+	}
 	
+	if(DEBUG) printf("[DiskDriver_readBlock] index of block to read : %d\n",block_num);
+	if(DEBUG) printf("[DiskDriver_readBlock] starting address : %p\n",disk->bitmap_data + disk->header->bitmap_entries + (block_num * BLOCK_SIZE));
 	memcpy(dest, disk->bitmap_data + disk->header->bitmap_entries + (block_num * BLOCK_SIZE), BLOCK_SIZE);
+
 	return 0;
 }
 
 
 int DiskDriver_writeBlock(DiskDriver* disk, void* src, int block_num) {
-	if (block_num >= disk->header->num_blocks)
+	if(!src || block_num >= disk->header->num_blocks)
 		return -1;
+	if(DEBUG) printf("\t[DiskDriver_writeBlock] write on block %d\n",block_num);
 
-	memcpy(disk->bitmap_data + disk->header->bitmap_entries + (block_num * BLOCK_SIZE), src, BLOCK_SIZE); // or disk_bitmap_data[block_num] + disk->header->bitmap_entries
+	BitMap bitmap;
+	bitmap.num_bits = disk->header->num_blocks;
+	bitmap.entries = disk->bitmap_data;
 
-	BitMap* bitmap;
-	bitmap->num_bits = disk->header->bitmap_blocks;
-	bitmap->entries = disk->bitmap_data;
 
-	if (BitMap_set(bitmap, block_num, 1) == -1)
+	int old_status = BitMap_set(&bitmap, block_num, 1);
+	if(old_status == -1)
 		return -1;
-	
-	disk->header->free_blocks--;
+	else if(old_status == 0)
+		disk->header->free_blocks--;
+
+	memcpy(disk->bitmap_data + disk->header->bitmap_entries + (block_num * BLOCK_SIZE), src, BLOCK_SIZE);
+
+	if (block_num == disk->header->first_free_block)
+		disk->header->first_free_block = BitMap_get(&bitmap, 0, 0);
+		
 	return 0;
 }
 
@@ -88,11 +131,11 @@ int DiskDriver_freeBlock(DiskDriver* disk, int block_num) {
 	if (block_num >= disk->header->num_blocks)
 		return -1;
 	
-	BitMap* bitmap;
-	bitmap->num_bits = disk->header->bitmap_blocks;
-	bitmap->entries = disk->bitmap_data;
+	BitMap bitmap;
+	bitmap.num_bits = disk->header->num_blocks;
+	bitmap.entries = disk->bitmap_data;
 
-	if (BitMap_set(bitmap, block_num, 0) == -1)
+	if (BitMap_set(&bitmap, block_num, 0) == -1)
 		return -1;
 
 	disk->header->free_blocks++;
@@ -111,22 +154,22 @@ int DiskDriver_getFreeBlock(DiskDriver* disk, int start) {
 	if (start < disk->header->first_free_block)
 		return disk->header->first_free_block;
 	
-	BitMap* bitmap;
-	bitmap->num_bits = disk->header->bitmap_blocks;
-	bitmap->entries = disk->bitmap_data;
+	BitMap bitmap;
+	bitmap.num_bits = disk->header->num_blocks;
+	bitmap.entries = disk->bitmap_data;
 
-	return BitMap_get(bitmap, start, 0);
+	return BitMap_get(&bitmap, start, 0);
 }
 
 
 int DiskDriver_flush(DiskDriver* disk) {
-    int ret;
-    int dd_size = sizeof(DiskHeader) + disk->header->bitmap_entries + disk->header->num_blocks * BLOCK_SIZE;
-    ret = msync(disk->header, dd_size, MS_ASYNC);
-    if (ret == -1)
-        return -1;
-    return 0;
+	int ret;
+	int dd_size = sizeof(DiskHeader) + disk->header->bitmap_entries + disk->header->num_blocks * BLOCK_SIZE;
+	if(DEBUG) printf("[DiskDriver_flush] dd_size : %d\n",dd_size);
+	if(DEBUG) printf("[DiskDriver_flush] mmapped at %p\n", disk->header);
+	ret = msync((void*)(disk->header), dd_size, MS_ASYNC);
+	if (ret == -1)
+	    return -1;
+	return 0;
 }
-
-
 
---
+++ disk_driver_test.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "bitmap.h"
+#include "disk_driver.h"
+
+int main(int agc, char** argv) {  
+  int err;
+  
+  DiskDriver dd;
+	DiskDriver_init(&dd, "fs", 200);
+	char junk[BLOCK_SIZE];
+	int i;
+	for(i=0;i<BLOCK_SIZE;i++)
+		junk[i] = 97+(i%21);
+	
+	err = DiskDriver_writeBlock(&dd, junk, 2);
+//	if(err==-1) printf("error in DiskDriver_writeBlock 0\n");
+	printf("line 19: DiskDriver_writeBlock(&dd, junk, 2);\n");
+	err = DiskDriver_writeBlock(&dd, junk, 2);
+	if(err!=-1) printf("OK DiskDriver_writeBlock 1: I should not have been blocked (even if block 2 already occupied)\n");
+	
+//	printf("DiskDriver_writeBlock tests passed\n");
+	
+	char dump[BLOCK_SIZE+1];
+	memset(dump,0,BLOCK_SIZE+1);
+	printf("line 27: DiskDriver_readBlock(&dd, dump, 4);\n");
+	err = DiskDriver_readBlock(&dd, dump, 4);
+	if(err!=-1) printf("DiskDriver_writeBlock 1: I should have been blocked (since block 4 is free)\n");
+	printf("line 30: DiskDriver_readBlock(&dd, dump, 2);\n");
+	err = DiskDriver_readBlock(&dd, dump, 2);
+	if(err==-1) printf("error in DiskDriver_readBlock 0\n");
+	
+	printf("read: %s\n", dump);
+	
+	if(memcmp(junk,dump,BLOCK_SIZE)!=0) printf("Incostincency, not reading what I wrote in\n");
+		
+	printf("line 38: DiskDriver_freeBlock(&dd, 2);\n");
+	err = DiskDriver_freeBlock(&dd, 2);
+	if(err==-1) printf("error freeing block 0\n");
+	err = DiskDriver_getFreeBlock(&dd, 2);
+	if(err==-1) printf("error DiskDriver_getFreeBlock block 0\n");
+	else printf("first_free_block from 2 is:%d(expected 2)\n",err);
+	
+	printf("line 45: DiskDriver_writeBlock(&dd, junk, 2);\n");
+	err = DiskDriver_writeBlock(&dd, junk, 2);
+	if(err==-1) printf("error in DiskDriver_writeBlock 0\n");
+	printf("line 48: DiskDriver_getFreeBlock(&dd, 2);\n");
+	err = DiskDriver_getFreeBlock(&dd, 2);
+	if(err==-1) printf("error DiskDriver_getFreeBlock block 0\n");
+	else printf("first_free_block from 2 is:%d(expected 3)\n",err);
+	
+	DiskDriver_flush(&dd);
+}
